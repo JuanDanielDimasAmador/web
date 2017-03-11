d01362742730596d94f5809f8e42f81908e618a0
JuanDanielDimasAmador <1715110271@utec-tgo.edu.mx>
2017-03-10 23:11:28 -0600

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,6 +1,6 @@
 <?php
-	$host = "localhost";
-	$user = "root";
-	$pw = "";
-	$db = "productos";
+	$host = "u3y93bv513l7zv6o.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+	$user = "spayzvuai6ekqb9v";
+	$pw = "mo7b7yk9r2bzkz2z ";
+	$db = "iag8jydz5y6ulbu";
 ?>
